86b4a56708bc04fa195c4573fd83d93748e955b0
ItsMeShisuii <54240189+ItsMeShisuii@users.noreply.github.com>
2019-08-20 13:28:07 +0200

    Working - Prefix Update

--- src/de/luca/soulgames/stickit/main/Main.java
+++ src/de/luca/soulgames/stickit/main/Main.java
@@ -3,7 +3,7 @@ package de.luca.soulgames.stickit.main;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-public static String PREFIX = "";
+public static String PREFIX = "SticktIT";
 
     @Override
     public void onEnable() {
