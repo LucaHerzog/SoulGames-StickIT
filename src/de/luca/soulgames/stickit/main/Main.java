15bc003cdb579f9ff32af20ce28e06b14edf3359
LucaHerzog <redimsky@gmail.com>
2019-08-20 13:22:04 +0200

    Working - StickIT, Main

---
+++ src/de/luca/soulgames/stickit/main/Main.java
@@ -0,0 +1,25 @@
+package de.luca.soulgames.stickit.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+public static String PREFIX = "";
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    public static void registerCommands() {
+
+    }
+
+    public static void registerEvents() {
+
+    }
+}
